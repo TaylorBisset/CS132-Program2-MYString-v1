0804282145191be54b96e107b180ec61de647108
Taylor Bisset <taylorbisset@gmail.com>
2023-10-01 12:50:30 -0700

    Copied my compareTo function from CS132-Program1-ComparingCstrings into TBString.h as private member.

--- CS132Program2-MYString-v1/TBString.h
+++ CS132Program2-MYString-v1/TBString.h
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-class TBString {    // You NEED to change the name of the class to reflect your name 
+class TBString 
+{    // You NEED to change the name of the class to reflect your name 
 					//   for example Nancy Programmer would use the name NPString 
 public:
 	TBString();						// default constructor
@@ -26,6 +27,21 @@ public:
 	const char* c_str();
 
 private:
+	int compareTo(const char* lstr, const char* rstr)
+	{
+		if (lstr < rstr)
+		{
+			return -1;
+		}
+		else if (lstr > rstr)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
 	char* str;
 	int end;
 	int cap;
