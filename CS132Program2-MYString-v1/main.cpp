d29d4280e2e164cc96641c6a4a885dc71a4b661a
Taylor Bisset <taylorbisset@gmail.com>
2023-10-01 14:00:55 -0700

    Complete console output.

--- CS132Program2-MYString-v1/main.cpp
+++ CS132Program2-MYString-v1/main.cpp
@@ -60,7 +60,14 @@ int main()
 
     // OUTPUT
 
-    
+    for (int i = 0; i < wordCnt; i++)
+    {
+        cout << left << setw(13) << words[i].c_str();
+        if ((i + 1) % 6 == 0)
+        {
+            cout << endl;
+        }
+    }
 
     // OS independent program termination sequence. 
 #ifdef _WIN32
